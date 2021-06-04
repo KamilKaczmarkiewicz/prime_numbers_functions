4806f4093d218648b8473e283e04ed98044502fb
unknown <kaczmarkiewicz98@gmail.com>
2021-06-04 22:12:36 +0200

    fixed 3rd function

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-#import numpy as np
 import time
-import matplotlib.pyplot as plt
-
 from numpy import sqrt
-from numpy import ceil
 
 def check_time(fun, n):
     start = time.time()
@@ -19,7 +15,6 @@ def first_function(n):
                 not_prime = False
                 break
         if not_prime: arr.append(i)    
-    #print(arr)
 
 def second_function(n):
     if n < 2 : return
@@ -39,30 +34,25 @@ def second_function(n):
                 break
         if first_not_prime: arr.append(a)
         if second_not_prime: arr.append(b)  
-    print(f"dla {n} jest {len(arr)} elementow")  
-    #print(arr)
-    #return(arr)
+    #print(f"dla {n} jest {len(arr)} elementow")  
 
 def third_function(n):
     if n < 2 : return
-    arr = {2, 3}
-    for i in range(6, n + 1, 6):
+    arr = [2, 3, 5, 7]
+    id = 2
+    for i in range(12, n + 1, 6):
         prime_a = True
         prime_b = True
-        arr2 = []
-        for a in arr:
-            arr2.append(a)
-            if a>=int(sqrt(i+1)+1): break
-        for a in arr2:
+        if arr[id] <= int(sqrt(i+1)+1): id += 1
+        for a in arr[:id:]:
             if (i-1)%a==0: 
                 prime_a = False
             if (i+1)%a==0:
                 prime_b = False
             if not prime_b and not prime_a: break
-        if prime_a: arr.add(i-1)
-        if prime_b: arr.add(i+1)
-    print(f"dla {n} jest {len(arr)} elementow")
-
+        if prime_a: arr.append(i-1)
+        if prime_b: arr.append(i+1)
+    #print(f"dla {n} jest {len(arr)} elementow")
 
 
 if __name__ == "__main__":
